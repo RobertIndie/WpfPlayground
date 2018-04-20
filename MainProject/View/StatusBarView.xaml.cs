d2d1fcf77edb0543c901ebae3e4d3935119b9f76
IIzzaya <linxuan_0709@icloud.com>
2018-04-20 15:54:42 +0800

    add DockSiteView & RibbonView

--- MainProject/View/StatusBarView.xaml.cs
+++ MainProject/View/StatusBarView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RE.MainProject
+namespace RE.MainProject.View
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// StatusBarView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StatusBarView : UserControl
     {
-        public MainWindow()
+        public StatusBarView()
         {
             InitializeComponent();
         }
